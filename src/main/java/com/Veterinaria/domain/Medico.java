1f30bd8be1240d9c6f91b58dc42cc79ec7b412a7
jmore15 <jonmoreira15@outlook.com>
2023-04-03 18:52:37 -0600

    Estructura Inicial Dao y controll

--- src/main/java/com/Veterinaria/domain/Medico.java
+++ src/main/java/com/Veterinaria/domain/Medico.java
@@ -1,5 +1,26 @@
 package com.Veterinaria.domain;
 
-public class Medico {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "medico")
+public class Medico implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private long idMedico;
+     private long idEspecialidad;
+    private String nombre;
+    private String apellido;
+
 
 }
